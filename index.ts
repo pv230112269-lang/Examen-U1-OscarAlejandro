66a1b8511d46f42f617aaf5dd307b1145034574e
PV230112269-LANG <pv230112269@vallarta.tecmm.edu.mx>
2026-02-18 16:43:34 -0700

    feat: logica para actualizar inventario despues de venta

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 let producto: string = "Pan";
 let cantidad: number = 10;
+let vendido: number = 3;
+
+cantidad = cantidad - vendido;
 
 console.log("Producto:", producto);
-console.log("Cantidad disponible:", cantidad);
+console.log("Cantidad restante:", cantidad);
